42f1d25f30c3e1d55370a5f2c422703d6562f2cb
Arcensoth <arcensoth@gmail.com>
2018-02-04 16:00:45 -0500

    Properly implement moderator/staff check

--- cogbot/checks.py
+++ cogbot/checks.py
@@ -1,25 +1,24 @@
 from discord.ext import commands
 from discord.ext.commands import Context
 
-from cogbot.cog_bot import CogBot
-
 
 def is_manager_check(ctx: Context):
-    bot = ctx.bot  # type: CogBot
-    return ctx.message.author.id in bot.state.managers
+    return ctx.message.author.id in ctx.bot.state.manager_ids
 
 
 def is_manager():
     return commands.check(lambda ctx: is_manager_check(ctx))
 
 
-def is_moderator_check(ctx: Context):
+def is_staff_check(ctx: Context):
     if is_manager_check(ctx):
         return True
 
-    # TODO check if user has appropriate server permissions
-    return False
+    author_roles = {role.id for role in ctx.message.author.roles}
+    author_staff_roles = author_roles.intersection(ctx.bot.state.staff_roles)
+
+    return bool(author_staff_roles)
 
 
-def is_moderator():
-    return commands.check(lambda ctx: is_moderator_check(ctx))
+def is_staff():
+    return commands.check(lambda ctx: is_staff_check(ctx))
