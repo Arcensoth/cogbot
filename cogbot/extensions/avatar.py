e1d84eff4cc6e7a86ed8c693b7144516453e747f
Arcensoth <arcensoth@gmail.com>
2018-11-20 02:19:23 -0500

    Implement simple extension that allows changing avatar

---
+++ cogbot/extensions/avatar.py
@@ -0,0 +1,23 @@
+import requests
+from discord.ext import commands
+from discord.ext.commands import Context
+
+from cogbot import checks
+from cogbot.cog_bot import CogBot
+
+
+class Avatar:
+    def __init__(self, bot):
+        self.bot: CogBot = bot
+
+    @checks.is_manager()
+    @commands.command(pass_context=True)
+    async def avatar(self, ctx: Context, *, url: str):
+        image_response = requests.get(url)
+        image_data = image_response.content
+        await self.bot.edit_profile(avatar=image_data)
+        await self.bot.react_success(ctx)
+
+
+def setup(bot):
+    bot.add_cog(Avatar(bot))
