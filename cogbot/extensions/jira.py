41eac7ec8f6d6f0d00454cc2127ffec6ab5e17a1
Arcensoth <arcensoth@gmail.com>
2018-11-21 10:21:23 -0500

    [jira] Make base URL configurable

--- cogbot/extensions/jira.py
+++ cogbot/extensions/jira.py
@@ -7,11 +7,17 @@ from discord.ext.commands import Context
 from cogbot.cog_bot import CogBot
 
 
+class JiraConfig:
+    def __init__(self, **options):
+        self.base_url = options['base_url']
+
+
 class Jira:
     REPORT_PATTERN = re.compile('^(mc-)?(\d+)$', re.IGNORECASE)
 
     def __init__(self, bot: CogBot, ext: str):
         self.bot = bot
+        self.config = JiraConfig(**bot.state.get_extension_state(ext))
 
     @commands.command(pass_context=True)
     async def jira(self, ctx: Context, *, query: str):
@@ -19,12 +25,11 @@ class Jira:
         if rmatch:
             rgroups = rmatch.groups()
             report_no = rgroups[1]
-            url = 'https://bugs.mojang.com/browse/MC-' + report_no
+            url = ''.join((self.config.base_url, '/browse/MC-', report_no))
         else:
             search_url = urllib.parse.urlencode({'searchString': query})
-            url = 'https://bugs.mojang.com/secure/QuickSearch.jspa?' + search_url
+            url = ''.join((self.config.base_url, '/secure/QuickSearch.jspa?', search_url))
         await self.bot.say(url)
-        await self.bot.react_success(ctx)
 
 
 def setup(bot):
