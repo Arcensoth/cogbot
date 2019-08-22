b43bcfe5e62d8be84c400f0ae86897bcbe60b18c
Arcensoth <arcensoth@gmail.com>
2019-08-21 21:55:06 -0400

    Paint it black

--- cogbot/cog_bot.py
+++ cogbot/cog_bot.py
@@ -19,7 +19,8 @@ class CogBot(commands.Bot):
             command_prefix=commands.when_mentioned_or(*state.command_prefix),
             description=state.description,
             help_attrs=state.help_attrs,
-            **options)
+            **options,
+        )
 
         self.state = state
 
@@ -35,24 +36,22 @@ class CogBot(commands.Bot):
         if self.state.extensions:
             self.load_extensions(*self.state.extensions)
         else:
-            log.info('No extensions to load')
+            log.info("No extensions to load")
 
-        log.info('Initialization successful')
+        log.info("Initialization successful")
 
     async def reply(self, content, *args, **kwargs):
-        author = kwargs.pop('author', _get_variable('_internal_author'))
-        destination = kwargs.pop('destination', _get_variable('_internal_channel'))
-        fmt = f'@{author.display_name} {content}'
+        author = kwargs.pop("author", _get_variable("_internal_author"))
+        destination = kwargs.pop("destination", _get_variable("_internal_channel"))
+        fmt = f"@{author.display_name} {content}"
 
-        extensions = ('delete_after',)
-        params = {
-            k: kwargs.pop(k, None) for k in extensions
-        }
+        extensions = ("delete_after",)
+        params = {k: kwargs.pop(k, None) for k in extensions}
 
         coro = self.send_message(destination, fmt, *args, **kwargs)
         sent_message = await self._augmented_msg(coro, **params)
 
-        fmt2 = f'{author.mention} {content}'
+        fmt2 = f"{author.mention} {content}"
 
         return await self.edit_message(sent_message, new_content=fmt2)
 
@@ -60,51 +59,54 @@ class CogBot(commands.Bot):
         self.queued_messages.append((dest_getter, dest_id, content))
 
     def load_extensions(self, *extensions):
-        log.info(f'Loading {len(extensions)} extensions...')
+        log.info(f"Loading {len(extensions)} extensions...")
         for ext in extensions:
-            log.info(f'Loading extension {ext}...')
+            log.info(f"Loading extension {ext}...")
             try:
                 self.load_extension(ext)
             except Exception as e:
-                log.exception(f'Failed to load extension {ext}')
-        log.info(f'Finished loading extensions')
+                log.exception(f"Failed to load extension {ext}")
+        log.info(f"Finished loading extensions")
 
     def unload_extensions(self, *extensions):
-        log.info(f'Unloading {len(extensions)} extensions...')
+        log.info(f"Unloading {len(extensions)} extensions...")
         for ext in extensions:
-            log.info(f'Unloading extension {ext}...')
+            log.info(f"Unloading extension {ext}...")
             try:
                 self.unload_extension(ext)
             except Exception as e:
-                log.exception(f'Failed to unload extension {ext}')
-        log.info(f'Finished unloading extensions')
+                log.exception(f"Failed to unload extension {ext}")
+        log.info(f"Finished unloading extensions")
 
     def force_logout(self):
         self._is_logged_in.clear()
 
     async def send_error(self, ctx: Context, destination, error: CommandError):
-        place = '' if ctx.message.server is None else f' on **{ctx.message.server}**'
-        reply = f'There was a problem with your command{place}: *{error.args[0]}*'
+        place = "" if ctx.message.server is None else f" on **{ctx.message.server}**"
+        reply = f"There was a problem with your command{place}: *{error.args[0]}*"
         await self.send_message(destination, reply)
 
     async def mod_log(self, ctx: Context, content: str, destination: Channel = None):
         await self.send_message(
             destination or self.mod_log_channel or ctx.message.author,
-            f'[{ctx.message.author}] {content}')
+            f"[{ctx.message.author}] {content}",
+        )
 
     async def on_ready(self):
-        log.info(f'Logged in as {self.user.name} (id {self.user.id})')
+        log.info(f"Logged in as {self.user.name} (id {self.user.id})")
 
         # Resolve mod log channel.
-        self.mod_log_channel = self.get_channel(self.state.mod_log) if self.state.mod_log else None
+        self.mod_log_channel = (
+            self.get_channel(self.state.mod_log) if self.state.mod_log else None
+        )
         if self.mod_log_channel:
-            log.info(f'Resolved mod log channel: #{self.mod_log_channel}')
+            log.info(f"Resolved mod log channel: #{self.mod_log_channel}")
         else:
-            log.warning(f'No mod log configured!')
+            log.warning(f"No mod log configured!")
 
         # Send any queued messages.
         if self.queued_messages:
-            log.info(f'Sending {len(self.queued_messages)} queued messages...')
+            log.info(f"Sending {len(self.queued_messages)} queued messages...")
             for dest_getter, dest_id, content in self.queued_messages:
                 dest = await dest_getter(dest_id)
                 await self.send_message(dest, content)
@@ -127,18 +129,18 @@ class CogBot(commands.Bot):
         if self.care_about_it(message):
             # listen to anyone on public channels
             if message.server:
-                log.info(f'[{message.server}/{message.author}] {message.content}')
+                log.info(f"[{message.server}/{message.author}] {message.content}")
                 await super().on_message(message)
 
             # listen only to managers on private (dm) channels
             elif message.author.id in self.state.managers:
-                log.info(f'[{message.author}] {message.content}')
+                log.info(f"[{message.author}] {message.content}")
                 await super().on_message(message)
 
     async def on_command_error(self, error: CommandError, ctx: Context):
         log.exception(
-            f'[{ctx.message.server}/{ctx.message.author}] {error.__class__.__name__}: {error.args[0]}',
-            exc_info=error
+            f"[{ctx.message.server}/{ctx.message.author}] {error.__class__.__name__}: {error.args[0]}",
+            exc_info=error,
         )
 
         inner_error = error.original if isinstance(error, CommandInvokeError) else error
@@ -163,22 +165,22 @@ class CogBot(commands.Bot):
             await self.react_poop(ctx)
 
     async def react_success(self, ctx: Context):
-        await self.add_reaction(ctx.message, u'✔')
+        await self.add_reaction(ctx.message, "✔")
 
     async def react_neutral(self, ctx: Context):
-        await self.add_reaction(ctx.message, u'➖')
+        await self.add_reaction(ctx.message, "➖")
 
     async def react_question(self, ctx: Context):
-        await self.add_reaction(ctx.message, u'❓')
+        await self.add_reaction(ctx.message, "❓")
 
     async def react_failure(self, ctx: Context):
-        await self.add_reaction(ctx.message, u'❗')
+        await self.add_reaction(ctx.message, "❗")
 
     async def react_denied(self, ctx: Context):
-        await self.add_reaction(ctx.message, u'🚫')
+        await self.add_reaction(ctx.message, "🚫")
 
     async def react_cooldown(self, ctx: Context):
-        await self.add_reaction(ctx.message, u'⏳')
+        await self.add_reaction(ctx.message, "⏳")
 
     async def react_poop(self, ctx: Context):
-        await self.add_reaction(ctx.message, u'💩')
+        await self.add_reaction(ctx.message, "💩")
