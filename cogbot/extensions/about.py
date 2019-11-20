c551923bb0f68172b1fedbae40ceb34942862682
Arcensoth <arcensoth@gmail.com>
2019-11-20 13:22:41 -0500

    Add contributors to about message

--- cogbot/extensions/about.py
+++ cogbot/extensions/about.py
@@ -1,9 +1,11 @@
+import json
 import logging
+import urllib.request
+
+from discord.ext import commands
+from discord.ext.commands import CommandError, Context
 
 from cogbot import checks
-from discord.ext import commands
-from discord.ext.commands import Context
-
 from cogbot.cog_bot import CogBot
 
 log = logging.getLogger(__name__)
@@ -12,6 +14,8 @@ BASE_REPOS = [
     "https://github.com/Arcensoth/cogbot",
     "https://github.com/Rapptz/discord.py"
 ]
+
+CONTRIBUTORS_FILE = "https://raw.githubusercontent.com/Arcensoth/cogbot/master/CONTRIBUTORS.json"
 
 
 class AboutConfig:
@@ -26,9 +30,8 @@ class About:
         options = bot.state.get_extension_state(ext)
         options['description'] = options.get('description', bot.description)
         self.config = AboutConfig(**options)
-        self.about_message = ''
 
-    async def make_about_message(self):
+    async def make_about_message(self, ctx: Context):
         parts = [
             self.config.description
         ]
@@ -44,37 +47,51 @@ class About:
                     manager_mention = manager
                 managers.append(manager_mention)
 
-            managers_str = '**Managed by**:\n    - ' + '\n    - '.join(m for m in managers)
+            managers_str = '**Managers**:\n    - ' + '\n    - '.join(managers)
             parts.append(managers_str)
+
+        contributors_data = {}
+        contributors = []
+
+        try:
+            response = urllib.request.urlopen(CONTRIBUTORS_FILE)
+            content = response.read().decode('utf8')
+            contributors_data  = json.loads(content)
+        except Exception as e:
+            raise CommandError('Failed to load contributors: {}'.format(e))
+
+        for contrib_data in contributors_data:
+            try:
+                contrib_name = contrib_data.get('name')
+                contrib_tag = contrib_data.get('tag')
+                contrib_id = contrib_data.get('id')
+                contrib_member = ctx.message.channel.server.get_member(contrib_id)
+                if contrib_member:
+                    contributors.append(contrib_member.mention)
+                else:
+                    contributors.append(contrib_tag or contrib_name)
+            except:
+                log.error(f'Failed to resolve contributor: {contrib_data}')
+
+        if contributors:
+            contributors_str = '**Contributors**:\n    - ' + '\n    - '.join(contributors)
+            parts.append(contributors_str)
 
         actual_repos = self.config.repos + BASE_REPOS
 
-        repos_str = '**Powered by**:\n    - <' + '>\n    - <'.join(repo for repo in actual_repos) + '>'
+        repos_str = '**Repositories**:\n    - <' + '>\n    - <'.join(repo for repo in actual_repos) + '>'
         parts.append(repos_str)
 
         about_message = '\n\n'.join(parts)
 
         return about_message
 
-    async def reload_about_message(self):
-        log.info('reloading about message...')
-        self.about_message = await self.make_about_message()
-        log.info('about message has been reloaded')
-
-    async def on_ready(self):
-        await self.reload_about_message()
-
     @commands.group(pass_context=True, name='about')
     async def cmd_about(self, ctx: Context):
         if ctx.invoked_subcommand is None:
+            text = await self.make_about_message(ctx)
             message = await self.bot.say('🤖')
-            await self.bot.edit_message(message, f'{self.about_message}')
-
-    @checks.is_manager()
-    @cmd_about.command(pass_context=True, name='reload', hidden=True)
-    async def cmd_about_reload(self, ctx: Context):
-        await self.reload_about_message()
-        await self.bot.react_success(ctx)
+            await self.bot.edit_message(message, text)
 
 
 def setup(bot):
