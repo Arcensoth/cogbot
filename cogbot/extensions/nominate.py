5a2d2e361529c2c6f4bedab063e65365bc5019e2
Arcensoth <arcensoth@gmail.com>
2020-01-18 15:09:14 -0500

    [nominate] Update noms and re-introduce message counting

--- cogbot/extensions/nominate.py
+++ cogbot/extensions/nominate.py
@@ -18,11 +18,14 @@ class NominateServerState:
         self,
         bot: CogBot,
         server: discord.Server,
+        recency_in_days: int,
         help_channels: typing.List[ChannelId],
         other_channels: typing.List[ChannelId],
     ):
         self.bot: CogBot = bot
         self.server: discord.Server = server
+
+        self.recency_in_days: int = recency_in_days
 
         self.help_channels: typing.List[discord.Channel] = [
             self.bot.get_channel(channel_id) for channel_id in help_channels
@@ -38,15 +41,15 @@ class NominateServerState:
         other_channels_str = ", ".join(str(channel) for channel in self.other_channels)
         log.info(f"Resolved other channels: {other_channels_str}")
 
-    # async def count_recent_messages(
-    #     self, channel: discord.Channel, member: discord.Member, since: datetime
-    # ) -> int:
-    #     # this is horribly inefficient but apparently there's no better way
-    #     # https://discordpy.readthedocs.io/en/v0.16.12/api.html#discord.Client.logs_from
-    #     # https://github.com/discordapp/discord-api-docs/issues/663
-    #     logs = self.bot.logs_from(channel, limit=999999999, after=since)
-    #     messages = [message async for message in logs if message.author == member]
-    #     return len(messages)
+    async def count_recent_messages(
+        self, channel: discord.Channel, member: discord.Member, since: datetime
+    ) -> int:
+        # this is horribly inefficient but apparently there's no better way
+        # https://discordpy.readthedocs.io/en/v0.16.12/api.html#discord.Client.logs_from
+        # https://github.com/discordapp/discord-api-docs/issues/663
+        logs = self.bot.logs_from(channel, limit=999999999, after=since)
+        messages = [message async for message in logs if message.author == member]
+        return len(messages)
 
     async def nominate(self, ctx: Context, user_id):
         # let the nominator know we're working on it
@@ -60,42 +63,56 @@ class NominateServerState:
             await self.bot.react_question(ctx)
             return
 
-        # use an objectively superior date format
-        join_date_text = member.joined_at.strftime("%Y/%m/%d")
-
-        # grab messages up to one month ago
-        # now = datetime.utcnow()
-        # one_month_ago = now - timedelta(days=30)
+        # grab recent messages
+        now = datetime.utcnow()
+        then = now - timedelta(days=self.recency_in_days)
 
         # sum up help messages first
-        # recent_help_messages = 0
-        # for channel in self.help_channels:
-        #     recent_help_messages += await self.count_recent_messages(
-        #         channel, member, since=one_month_ago
-        #     )
+        recent_help_messages = 0
+        for channel in self.help_channels:
+            recent_help_messages += await self.count_recent_messages(
+                channel, member, since=then
+            )
 
         # then sum up total messages
-        # total_recent_messages = recent_help_messages
-        # for channel in self.other_channels:
-        #     total_recent_messages += await self.count_recent_messages(
-        #         channel, member, since=one_month_ago
-        #     )
-
-        # calculate the help ratio
-        # if total_recent_messages > 0:
-        #     help_ratio = round(100 * recent_help_messages / total_recent_messages)
-        #     help_ratio_text = f"{help_ratio}% of {total_recent_messages}"
-        # else:
-        #     help_ratio_text = r"¯\_(ツ)_/¯"
+        total_recent_messages = recent_help_messages
+        for channel in self.other_channels:
+            total_recent_messages += await self.count_recent_messages(
+                channel, member, since=then
+            )
 
         # create an embed with clickable mention
-        em = discord.Embed(description=f"{member.mention}", color=0x00ACED)
+        em = discord.Embed(
+            description=f"Nominations for: {member.mention}",
+            color=0x00ACED,
+            timestamp=member.joined_at,
+        )
+        em.set_footer(text="Joined the server", icon_url=member.avatar_url)
         em.set_thumbnail(url=member.avatar_url)
-        em.add_field(name="Join date", value=join_date_text)
-        # em.add_field(name="Recent help", value=help_ratio_text)
 
-        # send the embed
+        # add the total number of recent messages
+        em.add_field(
+            name="Number of recent messages",
+            value=f"{total_recent_messages} in {self.recency_in_days} days",
+        )
+
+        # and the help ratio, if any
+        if total_recent_messages > 0:
+            help_ratio = round(100 * recent_help_messages / total_recent_messages)
+            em.add_field(
+                name="Ratio of messages in help-chats",
+                value=f"{help_ratio}% of {total_recent_messages}",
+            )
+        else:
+            em.add_field(name="Ratio of messages in help-chats", value=r"¯\_(ツ)_/¯")
+
+        # send the message with embed
         nom_message = await self.bot.send_message(ctx.message.channel, embed=em)
+
+        # edit-mention and add a username to the message content, just in case
+        # people's clients bug out with the embeds
+        content = f"{member.mention} ({member.name}#{member.discriminator})"
+        await self.bot.edit_message(nom_message, content)
 
         # add some default reactions
         await self.bot.add_reaction(nom_message, "👍")
