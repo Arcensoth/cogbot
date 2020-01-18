1d9e0c6f3bbb3ca6019955ca0f33ab8da84b9eba
Arcensoth <arcensoth@gmail.com>
2020-01-18 17:48:54 -0500

    [nick] Added config and made opt-in only

--- cogbot/extensions/nick.py
+++ cogbot/extensions/nick.py
@@ -1,4 +1,5 @@
 import logging
+import typing
 
 import discord
 
@@ -7,18 +8,42 @@ from cogbot.cog_bot import CogBot
 log = logging.getLogger(__name__)
 
 
-class Nick:
-    def __init__(self, bot):
+class NickServerState:
+    def __init__(self, bot: CogBot, server: discord.Server, enabled: bool = False):
         self.bot: CogBot = bot
+        self.server: discord.Server = server
+        self.enabled: bool = enabled
+
+
+class Nick:
+    def __init__(self, bot: CogBot, ext: str):
+        self.bot: CogBot = bot
+        self.server_state: typing.Dict[discord.Server, NickServerState] = {}
+        self.options = self.bot.state.get_extension_state(ext)
+
+    def get_state(self, server: discord.Server) -> NickServerState:
+        return self.server_state.get(server)
+
+    async def on_ready(self):
+        # construct server state objects for easier context management
+        for server_key, server_options in self.options.get("servers", {}).items():
+            server = self.bot.get_server_from_key(server_key)
+            if server:
+                state = NickServerState(self.bot, server, **server_options)
+                self.server_state[server] = state
 
     def needs_correction(self, nick: str) -> bool:
         return nick and nick.startswith("!")
 
     async def on_member_update(self, before: discord.Member, after: discord.Member):
-        if self.needs_correction(after.nick):
+        state = self.get_state(after.server)
+        if state and state.enabled and self.needs_correction(after.nick):
             log.info(f"Correcting member nickname: {after.nick}")
-            await self.bot.change_nickname(after, f"\u17b5{after.nick}")
+            try:
+                await self.bot.change_nickname(after, f"\u17b5{after.nick}")
+            except:
+                log.info(f"Couldn't correct member's nickname: {after.nick}")
 
 
 def setup(bot):
-    bot.add_cog(Nick(bot))
+    bot.add_cog(Nick(bot, __name__))
