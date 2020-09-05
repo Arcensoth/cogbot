936dcd01b7cb84d2c8d0ce9fb5f791ebd61a1423
Arcensoth <arcensoth@gmail.com>
2020-09-05 17:19:16 -0400

    [robo_mod] Add delay to embed/attachment detection

--- cogbot/cogs/robo_mod/conditions/message_has_attachment.py
+++ cogbot/cogs/robo_mod/conditions/message_has_attachment.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from cogbot.cogs.robo_mod.robo_mod_condition import RoboModCondition
 from cogbot.cogs.robo_mod.robo_mod_trigger import RoboModTrigger
 
@@ -5,9 +7,13 @@ from cogbot.cogs.robo_mod.robo_mod_trigger import RoboModTrigger
 class MessageHasAttachmentCondition(RoboModCondition):
     def __init__(self):
         self.min_count: int
+        self.delay: int
 
     async def update(self, state: "RoboModServerState", data: dict):
         self.min_count = data.get("min_count", 1)
+        self.delay = data.get("delay", 2000)
 
     async def check(self, trigger: RoboModTrigger) -> bool:
+        # Give the client cache some time to update.
+        await asyncio.sleep(self.delay / 1000)
         return len(trigger.message.attachments) >= self.min_count
--- cogbot/cogs/robo_mod/conditions/message_has_embed_or_attachment.py
+++ cogbot/cogs/robo_mod/conditions/message_has_embed_or_attachment.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from cogbot.cogs.robo_mod.robo_mod_condition import RoboModCondition
 from cogbot.cogs.robo_mod.robo_mod_trigger import RoboModTrigger
 
@@ -5,11 +7,15 @@ from cogbot.cogs.robo_mod.robo_mod_trigger import RoboModTrigger
 class MessageHasEmbedOrAttachmentCondition(RoboModCondition):
     def __init__(self):
         self.min_count: int
+        self.delay: int
 
     async def update(self, state: "RoboModServerState", data: dict):
         self.min_count = data.get("min_count", 1)
+        self.delay = data.get("delay", 2000)
 
     async def check(self, trigger: RoboModTrigger) -> bool:
+        # Give the client cache some time to update.
+        await asyncio.sleep(self.delay / 1000)
         count_embeds = len(trigger.message.embeds)
         count_attachments = len(trigger.message.attachments)
         count = count_embeds + count_attachments
