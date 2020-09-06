1e4c859dcf3c72e93328ab919b54e684e5b98f52
Arcensoth <arcensoth@gmail.com>
2020-09-06 19:15:01 -0400

    [robo_mod] Fix unbanned trigger

--- cogbot/cogs/robo_mod/triggers/member_unbanned.py
+++ cogbot/cogs/robo_mod/triggers/member_unbanned.py
@@ -1,14 +1,21 @@
 from typing import Optional
 
-from discord import Member
+from discord import Member, Server
 
 from cogbot.cogs.robo_mod.robo_mod_rule import RoboModRule
 from cogbot.cogs.robo_mod.robo_mod_trigger import RoboModTrigger
 
 
 class MemberUnbannedTrigger(RoboModTrigger):
-    def __init__(self, state: "RoboModServerState", rule: RoboModRule, member: Member):
+    def __init__(
+        self,
+        state: "RoboModServerState",
+        rule: RoboModRule,
+        server: Server,
+        member: Member,
+    ):
         super().__init__(state, rule)
+        self._server: Server = server
         self._member: Member = member
 
     @property
