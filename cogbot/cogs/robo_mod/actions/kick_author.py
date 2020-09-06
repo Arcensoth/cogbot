94b800f53aabea7650642e13f86789864fb3f532
Arcensoth <arcensoth@gmail.com>
2020-09-06 16:52:28 -0400

    [robo_mod] use logging for kick action

--- cogbot/cogs/robo_mod/actions/kick_author.py
+++ cogbot/cogs/robo_mod/actions/kick_author.py
@@ -1,10 +1,13 @@
+from typing import Optional
+
 from cogbot.cogs.robo_mod.robo_mod_action import RoboModAction
+from cogbot.cogs.robo_mod.robo_mod_action_log_entry import RoboModActionLogEntry
 from cogbot.cogs.robo_mod.robo_mod_trigger import RoboModTrigger
 
 
 class KickAuthorAction(RoboModAction):
+    async def log(self, trigger: RoboModTrigger) -> Optional[RoboModActionLogEntry]:
+        return RoboModActionLogEntry(content=f"{trigger.author.mention} got the 👢!")
+
     async def apply(self, trigger: RoboModTrigger):
         await trigger.bot.kick(trigger.author)
-        await trigger.bot.send_message(
-            trigger.channel, content=f"{trigger.author.mention} got the 👢!"
-        )
